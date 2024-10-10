3311b80a6ccc42f2aed7c98680d7fec5f36c148b
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-10-09 21:29:41 -0300

    Update operadoresAritmeticos.cs

--- Fundamentos/operadoresAritmeticos.cs
+++ Fundamentos/operadoresAritmeticos.cs
@@ -1,1 +1,28 @@
 
+using System;
+
+namespace MeuApp
+{
+    class Operadores
+    {
+        static void Main (string[] args)
+        {
+
+            int soma = 25 + 22;
+            int subtracao = 25 - 22; 
+            int divisao = 25 * 22;
+            int multiplicacao = 22 / 5;
+
+            Console.WriteLine(soma);
+            Console.WriteLine(subtracao);
+            Console.WriteLine(divisao);
+            Console.WriteLine(multiplicacao);
+
+
+
+
+
+
+        }
+    }
+}

1e66384bbca1cc5d1dbb5327d6041f4d0d17e29f
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-10-13 14:13:31 -0300

    Update operadoresAritmeticos.cs

--- Fundamentos/operadoresAritmeticos.cs
+++ Fundamentos/operadoresAritmeticos.cs
@@ -1,26 +1,16 @@
-
 using System;
 
-namespace MeuApp
+namespace MyApp 
 {
-    class Operadores
+    internal class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 2; // % Representa o modulo da divisão
 
-            int soma = 25 + 22;
-            int subtracao = 25 - 22; 
-            int divisao = 25 * 22;
-            int multiplicacao = 22 / 5;
-
-            Console.WriteLine(soma);
-            Console.WriteLine(subtracao);
-            Console.WriteLine(divisao);
-            Console.WriteLine(multiplicacao);
-
-
-
-
+            double n4 = (double) 5 / 2; // Mesmo que eu declare como double, o compilador vai retornar um numero inteiro pois pedi divisão de numeros inteiros, então preciso fazer um Casting
 
 
         }
